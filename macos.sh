43d1e6daec0a0a4cb0d0f1141a3afe6d2ec523e1
Max Gallo <max.gallo@Maxs-MacBook-Pro.local>
2016-09-23 17:31:06 +0100

    adding some cool macos defaults

--- macos.sh
+++ macos.sh
@@ -1,1 +1,38 @@
+# Dark mode and reduce transparency
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 
+# Hot Corners
+# Top left → Mission control
+defaults write com.apple.dock wvous-tl-corner -int 2
+defaults write com.apple.dock wvous-tl-modifier -int 0
+# Top right → Application Windows
+defaults write com.apple.dock wvous-tr-corner -int 3
+defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left → Desktop
+defaults write com.apple.dock wvous-bl-corner -int 4 
+defaults write com.apple.dock wvous-bl-modifier -int 0
+# Bottom right → Sleep
+defaults write com.apple.dock wvous-br-corner -int 10 
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+
+# Don’t open Photos when plugging in an iPhone
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Use list view in all Finder windows by default
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Enable full keyboard access for all controls
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+
+# Kill all affected apps
+for app in "cfprefsd" "Dock" "Finder" "Mail" "Safari" "SystemUIServer"; do
+	killall "${app}" > /dev/null 2>&1
+done
