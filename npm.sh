35c18606da452c776e3fc8a30904b4db220a5764
Max Gallo <max.gallo@L01007180.local>
2017-01-09 15:01:23 +0000

    npm is now executable

--- npm.sh
+++ npm.sh
@@ -1,1 +1,3 @@
-npm i -g eslint babel-eslint eslint-config-airbnb eslint-plugin-jsx-a11y@^2.2.3 eslint-plugin-import eslint-plugin-react
+#!/bin/bash
+npm i -g eslint babel-eslint eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react
+
