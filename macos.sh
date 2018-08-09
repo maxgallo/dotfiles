19e8bf2c5852e1dd47688ca036a77c27c564c9fc
Max Gallo <pietropeluso@pietros-mbp.home>
2018-08-09 20:35:35 +0100

    updating the macos default settings

--- macos.sh
+++ macos.sh
@@ -1,36 +1,67 @@
-# Dark mode and reduce transparency
-defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+# Setting default location to save screenshots
+defaults write com.apple.screencapture location ~/Documents/Screenshots
 
 # Hot Corners
-# Top left → Mission control
-defaults write com.apple.dock wvous-tl-corner -int 2
+# Top left → Desktop
+defaults write com.apple.dock wvous-tl-corner -int 4
 defaults write com.apple.dock wvous-tl-modifier -int 0
-# Top right → Application Windows
-defaults write com.apple.dock wvous-tr-corner -int 3
+# Top right → Mission Control
+defaults write com.apple.dock wvous-tr-corner -int 2
 defaults write com.apple.dock wvous-tr-modifier -int 0
 # Bottom left → Desktop
-defaults write com.apple.dock wvous-bl-corner -int 4 
+defaults write com.apple.dock wvous-bl-corner -int 3 
 defaults write com.apple.dock wvous-bl-modifier -int 0
 # Bottom right → Sleep
 defaults write com.apple.dock wvous-br-corner -int 10 
 defaults write com.apple.dock wvous-br-modifier -int 0
 
+# Show remaining battery percentage
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
-# Don’t open Photos when plugging in an iPhone
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
+# Show Path bar in Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
+# Disable the 'Are you sure you want to open this application?' dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable auto-correct
+# defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Disable Safari’s thumbnail cache for History and Top Sites
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Disable send and reply animations in Mail.app
+# defaults write com.apple.Mail DisableReplyAnimations -bool true
+# defaults write com.apple.Mail DisableSendAnimations -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Don’t open Photos when plugging in an iPhone
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
 # Use list view in all Finder windows by default
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-# Enable full keyboard access for all controls
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
 
 # Git, use vim as editor
 git config --global core.editor `which vim`
