24516abcd28cba6f9c266ab9f8251f5275c1e7f5
Max Gallo <max.gallo@dazn.com>
2019-01-27 19:42:45 +0000

    fixes duplicate echo

--- macos.sh
+++ macos.sh
@@ -42,7 +42,7 @@ defaults write com.apple.dock autohide -bool false
 ###############################################################################
 # Trackpad & keyboard                                                         #
 ###############################################################################
-echo "Configuring Dock"
+echo "Configuring Trackpad & Keyboard"
 
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
