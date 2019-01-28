e72050e6cfd82599d1ec1f302bf95e3c9e893279
Max Gallo <max.gallo@dazn.com>
2019-01-28 01:08:15 +0000

    nvm is now fully working

--- nvm.sh
+++ nvm.sh
@@ -3,6 +3,7 @@
 source ./utils/confirm.sh
 source ./utils/config.sh
 source ./utils/check.sh
+source ./utils/log.sh
 
 check "brew" || exit
 check "fish" || (echo "Did you forget to run ./fish.sh first?" ; exit)
@@ -12,15 +13,20 @@ if [ "$1" == "--remove" ] || [ "$1" == "-r" ]; then
 
     echo "Uninstalling nvm"
     rm -rf ~/.nvm/
+
     echo "omf uninstall nvm" | fish
 
     exit
 fi
 
-echo "Installing nvm"
+logStep "Installing nvm"
+mkdir -p ~/.nvm/
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 
-echo "Installing nvm for Oh My Fish"
+
+logStep "Installing nvm for Oh My Fish"
+echo "omf uninstall nvm" | fish
 echo "omf install nvm" | fish
 
+logStep "Installing node 8.10"
 echo "nvm install 8.10" | fish
