5eadd81c0838ad78973f9bc7eba14e76ae9d0c2c
Max Gallo <max.gallo@dazn.com>
2019-01-28 02:46:39 +0000

    using logStep inside tmux

--- tmux.sh
+++ tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ./utils/confirm.sh
 source ./utils/config.sh
+source ./utils/log.sh
 
 if [ "$1" == "--remove" ] || [ "$1" == "-r" ]; then
     confirm "Are you sure you want to uninstall Tmux and reset the configuration?" || exit
@@ -14,10 +15,10 @@ if [ "$1" == "--remove" ] || [ "$1" == "-r" ]; then
     exit
 fi
 
-echo "Installing Tmux"
+logStep "Installing Tmux"
 brew install tmux
 
-echo "Symlinking .tmux.conf file"
+logStep "Symlinking .tmux.conf file"
 rm ~/.tmux.conf
 ln -s "$dotfiles_folder/tmux/.tmux.conf" ~/.tmux.conf
 
