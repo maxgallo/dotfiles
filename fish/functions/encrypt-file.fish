9fea21e4c87c627252d6d777d2546f1f2651ad6d
Max Gallo <max.gallo@dazn.com>
2020-05-01 10:48:47 +0100

    improve output of encrypt-file

--- fish/functions/encrypt-file.fish
+++ fish/functions/encrypt-file.fish
@@ -1,3 +1,6 @@
 function encrypt-file
     openssl aes-256-cbc -a -salt -base64 -in $argv[1] -out $argv[2]
+
+    echo use the following to decrypt:
+    echo openssl aes-256-cbc -a -d -salt -base64 -in $argv[2] -out $argv[2].decrypted
 end
