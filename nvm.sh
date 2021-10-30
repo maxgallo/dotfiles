fbcbd3b48ee4eeff61fc62b19f6a6ce2626c4879
Max Gallo <max.gallo@dazn.com>
2021-10-30 20:00:46 +0100

    feat: reduce fish load time from 1s to 80ms

--- nvm.sh
+++ nvm.sh
@@ -14,7 +14,8 @@ if [ "$1" == "--remove" ] || [ "$1" == "-r" ]; then
     echo "Uninstalling nvm"
     rm -rf ~/.nvm/
 
-    echo "omf uninstall nvm" | fish
+    echo "omf uninstall https://github.com/edc/bass" | fish
+    echo "omf uninstall https://github.com/fabioantunes/fish-nvm" | fish
 
     exit
 fi
@@ -23,9 +24,22 @@ logStep "Installing nvm"
 mkdir -p ~/.nvm/
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-logStep "Installing nvm for Oh My Fish"
-echo "omf uninstall nvm" | fish
-echo "omf install nvm" | fish
+# This causes fish shell to take between 0.5s and 1s to load.
+#
+# You can test yourself with
+#   time fish -c "echo hello"
+# and check the differencies with ~10ms of
+#   time bash -c "echo hello"
+#
+# This affect not only opening a new shell, but using commands like `fzf`
+# logStep "Installing nvm for Oh My Fish"
+# echo "omf uninstall nvm" | fish
+# echo "omf install nvm" | fish
+#
+# To fix the problem, this was created https://github.com/FabioAntunes/fish-nvm
+# now the command above in fish, takes ~70ms
+echo "omf install https://github.com/fabioantunes/fish-nvm" | fish
+echo "omf install https://github.com/edc/bass" | fish
 
 logStep "Installing latest node"
 echo "nvm install (nvm ls-remote | tail -n 1)" | fish
