440a7de079983aa9534080567aa7a6c294f7cc86
Max Gallo <max.gallo@dazn.com>
2025-12-18 11:41:47 +0100

    feat: remove powerline

--- powerline/fish_prompt.fish
+++
@@ -1,8 +0,0 @@
-# Powerline go
-# https://github.com/justjanne/powerline-go
-# - much faster than "powerline" or "powerline-shell"
-
-function fish_prompt
-    eval ~/bin/powerline-go -error $status -shell bare -hostname-only-if-ssh -modules "venv,host,ssh,cwd,perms,git,hg,jobs,exit,root"
-end
-
